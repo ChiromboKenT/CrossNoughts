e6c2a547e1a4f15b2366f1693a787dc8d50501fa
ChiromboKenT <chirombokenny@gmail.com>
2021-10-05 21:56:34 +0200

    Game Logic

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ class GameState():
         self.player = currentPlayer
         self.level = level
         self.GameBoard = Board(3,3,self.get_bg())
-        self.gameResult = 0
         
 
     def get_bg(self):
@@ -60,6 +59,8 @@ class Board():
         self.boardBackground = boardImage
         self.boardResult = 0
 
+    def reset(self,rows,columns):
+        self.board = np.zeros((rows,columns))
     def playerToken(self,row,col,player):
         if(self.isValidPosition(row,col)):
             self.board[row][col] = player
@@ -67,7 +68,7 @@ class Board():
                 self.GameOver(player)
                 return False
         if(self.isBoardFull()):
-            self.GameOver(0)
+            self.GameOver(2)
             return False
         return True
 
@@ -95,12 +96,13 @@ class Board():
                 return self.boardResult
         return 0
     
-    def draw_rect_alpha(self,surface, color, pos):
-        shape_surf = pygame.Surface((600,440), pygame.SRCALPHA)
+    def draw_rect_alpha(self,surface, color, pos,size):
+        shape_surf = pygame.Surface(size, pygame.SRCALPHA)
         shape_surf.fill(color)
         surface.blit(shape_surf, pos)
 
     def renderBoard(self,Window):
+        smallfont = pygame.font.SysFont('Corbel',35)
         Window.blit(self.boardBackground, (0,0))
         ##Draw Horizontal Lines
         pygame.draw.aaline(Window, (0,0,0), [xOffset,yOffset + 145], [800- xOffset,yOffset + 145], True)
@@ -110,7 +112,7 @@ class Board():
         pygame.draw.aaline(Window, (0,0,0), [xOffset + 400, yOffset], [xOffset + 400,600 - yOffset], True)
 
         #Draw Transparent Rectangle
-        self.draw_rect_alpha(Window,(67,0,56,140), [xOffset,yOffset])
+        self.draw_rect_alpha(Window,(67,0,56,140), [xOffset,yOffset],(600,440))
 
         
         for cellIndex, cellValue in np.ndenumerate(self.board):
@@ -119,8 +121,32 @@ class Board():
             elif(cellValue == playerCircle):
                 Window.blit(Circle_img,((cellIndex[1] * 200) + xOffset + 28.5, (cellIndex[0] * 145) + yOffset))
 
+        
+        if(GameState.gameResult != 0):
+            text = smallfont.render('Play Again?' , True , (255,255,255))
+            if(GameState.gameResult == playerCircle):
+                winner = "Player 1 Won!"
+            elif(GameState.gameResult == playerCross):
+                winner = "Player 2 Won!"
+            else:
+                winner = "Draw!!"
+            
+            textWinner = smallfont.render(f"{winner}",True,(255,255,255))
+            
+            self.draw_rect_alpha(Window,(4,170,109,110), [0,0],(800,600))
+            
+            self.draw_rect_alpha(Window,(0,75,188,255), [xOffset + 100,yOffset + 100],(400,100))
+            self.draw_rect_alpha(Window,(67,0,56,255), [xOffset + 200,yOffset + 300],(200,50))
+            
+            Window.blit(textWinner, [xOffset + 300 - textWinner.get_width()/2,yOffset + 150 - textWinner.get_height()/2])
+            Window.blit(text, [xOffset + 300 - text.get_width()/2,yOffset + 325 - text.get_height()/2])
+
     def GameOver(self,result):
+        GameState.gameResult = result
         print(f'Result : {result}')
+        
+        pygame.display.update()
+
 def re_Render(Board):
     rectangleCoord = pygame.Rect(xOffset,yOffset,600,440)
     Board.renderBoard(mainWindow)
@@ -144,17 +170,13 @@ gameState = GameState(playerCross,GameLevel)
 currentBoard = gameState.GameBoard
 gameRunning = True
 
-    
-def GameOver():
-    print(f'Winner = {gameState}')
-    pass
-     
+        
 
 PlayerTurn = 1
 CurrentPlayer = 0
 clicked = False
 nextTurn = 1
-
+winButtonCoord =  pygame.Rect((xOffset + 178,yOffset + 278),(250,80))
 #Game Loop
 while gameRunning:
     CurrentPlayer = PlayerTurn
@@ -168,11 +190,19 @@ while gameRunning:
         if event.type == MOUSEBUTTONDOWN:
             clicked = True
         if event.type == MOUSEBUTTONUP and clicked == True:
+            
+            if(pygame.Rect.collidepoint(winButtonCoord, event.pos) and GameState.gameResult != 0):
+                currentBoard.reset(3,3)
+                PlayerTurn = 1
+                nextTurn = 1
+                CurrentPlayer = 0
+                GameState.gameResult = 0
             if(nextTurn):
                 row = mt.floor((yOffset + event.pos[1])/146) - 1
                 column = mt.floor((xOffset + event.pos[0])/200) -1
                 nextTurn = currentBoard.playerToken(row,column,CurrentPlayer)
                 PlayerTurn *= -1
+            clicked = False
             
     mainWindow.fill((0,0,0))
     #Display Update
